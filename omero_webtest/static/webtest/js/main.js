eecc7570902bc163726e3f544764d9250fa908b6
Rodrigo Rosas-Bertolini <rodrigo.rosas3@gmail.com>
2024-07-24 09:35:00 +0200

    Issue with the scripts not being called anymore due to href issue addressed This required a lot of changes in both html and js The menus dont look anywhere right still. a lot of work to do in the standalone widget

--- omero_webtest/static/webtest/js/main.js
+++ omero_webtest/static/webtest/js/main.js
@@ -6,7 +6,8 @@ jQueryNoConflict(document).ready(function($) {
     // Function to fetch and render the script menu
     function fetchScriptMenu() {
         var url = $("#draggable").data("url"); // Get the URL from the data attribute
-        console.log("Fetching script menu from URL:", url); // Log the URL
+
+        console.log("Fetching script menu from URL:", url);
 
         // Make an AJAX GET request to fetch the script menu
         $.ajax({
@@ -18,33 +19,14 @@ jQueryNoConflict(document).ready(function($) {
 
                 // Check the structure of the response
                 if (Array.isArray(response)) {
-                    var scriptMenuHtml = buildScriptMenuHtml(response);
-                    $("#draggable").html(scriptMenuHtml);
+                    var omeroHtml = buildScriptMenuHtml(response.filter(folder => folder.name === "omero"));
+                    var biomeroHtml = buildScriptMenuHtml(response.filter(folder => folder.name === "biomero"));
 
-                    // Bind click event to script links to use OME.openScriptWindow
-                    $("#draggable").on('click', 'a', function(event) {
-                        var $a = $(event.target),
-                            script_url = $a.attr('href');
-                        if (script_url != "#") {
-                            // Clicked on script - handled by OME.openScriptWindow
-                            event.preventDefault();
-                            OME.openScriptWindow(event);
-                            return false;
-                        }
+                    $("#omero").html(omeroHtml);
+                    $("#biomero").html(biomeroHtml);
 
-                        // we have clicked on <a> within a <li>, with sibling <ul>
-                        var $li = $a.parent(),
-                            $ul = $li.children('ul');
-                        if ($li.hasClass('menu_back')) {
-                            $li.parent().parent().siblings().show();
-                            $li.parent().hide();
-                            $li.parent().siblings('a').show();
-                        } else {
-                            $ul.show();
-                            $li.siblings().hide();
-                            $a.hide();
-                        }
-                    });
+                    // Show the default tab (omero)
+                    openTab(null, 'omero');
                 } else {
                     console.error("Unexpected response format:", response);
                     $("#draggable").html("<p>Error loading script menu.</p>");
@@ -63,30 +45,59 @@ jQueryNoConflict(document).ready(function($) {
 
     // Function to build the script menu HTML
     function buildScriptMenuHtml(scriptMenu) {
-        var html = '<div class="script-menu"><ul>';
-        scriptMenu.forEach(function(folder) {
-            html += '<li><a href="#" title="' + folder.name + '">' + folder.name + '</a>';
-            if (Array.isArray(folder.ul)) {
-                html += '<ul>';
-                folder.ul.forEach(function(item) {
-                    if (item.id) {
-                        html += '<li><a href="/webclient/script_ui/' + item.id + '/">' + item.name + '...</a></li>';
-                    } else {
-                        html += '<li class="menuItem"><a href="#">' + item.name + '</a>';
-                        if (Array.isArray(item.ul) && item.ul.length > 0) {
-                            html += '<ul>' + buildScriptMenuHtml(item.ul) + '</ul>';
-                        }
-                        html += '</li>';
-                    }
-                });
-                html += '</ul>';
+        var html = '';
+        scriptMenu.forEach(function(item) {
+            if (item.id) {
+                // Leaf node (script)
+                html += '<div class="script-card"><a href="/webclient/script_ui/' + item.id + '/" class="script-link">' + item.name + '</a></div>';
+            } else {
+                // Directory node
+                html += '<div class="subdirectory-header">' + item.name + '</div>';
+                html += '<div class="script-cards-container">' + buildScriptMenuHtml(item.ul) + '</div>';
             }
-            html += '</li>';
         });
-        html += '</ul></div>';
         return html;
+    }
+
+    // Function to open a tab
+    window.openTab = function(event, tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        if (event) {
+            event.currentTarget.className += " active";
+        }
+    }
+
+    // Function to search scripts
+    window.searchScripts = function() {
+        var input, filter, scriptCards, i;
+        input = document.getElementById("searchBar");
+        filter = input.value.toLowerCase();
+        scriptCards = document.getElementsByClassName("script-card");
+        for (i = 0; i < scriptCards.length; i++) {
+            if (scriptCards[i].innerHTML.toLowerCase().indexOf(filter) > -1) {
+                scriptCards[i].style.display = "";
+            } else {
+                scriptCards[i].style.display = "none";
+            }
+        }
     }
 
     // Fetch and render the script menu on page load
     fetchScriptMenu();
+
+    // Bind click event to script links to use OME.openScriptWindow
+    $("#draggable").on('click', 'a.script-link', function(event) {
+        event.stopPropagation(); // Prevent the event from bubbling up
+        event.preventDefault(); // Prevent the default link behavior
+        OME.openScriptWindow(event); // Call the function to open the script window
+    });
 });
